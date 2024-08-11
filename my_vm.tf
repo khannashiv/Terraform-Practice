d7b105fd3deec63da3be71044389bbc17e1de671
Shiv <84401719+khannashiv@users.noreply.github.com>
2024-08-11 05:31:19 +0530

    Add files via upload

---
+++ my_vm.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+    secret_key = "kpuK0flmVLPCkA9KoXqZ2FPV3lbvGLDen8C7gxqU"
+    access_key = "AKIAQSUSBTMJGDWF3US3"
+  
+}
+
+
+resource "aws_instance" "my-ec2-vm" {
+  ami           =  "ami-03972092c42e8c0ca"
+  instance_type =  "t2.micro"
+}
