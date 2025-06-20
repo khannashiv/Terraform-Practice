49e2a0cbb5c8077bd52a9f0c6edb1fe5e98b0480
Shiv <khannashiv94@gmail.com>
2025-06-20 19:17:04 +0530

    test

---
+++ Projects/Project-1/main.tf
@@ -0,0 +1,48 @@
+resource "aws_vpc" "vpc-1" {
+  cidr_block=var.vpc_cidr
+  tags = {
+    Name = "My-VPC"
+  }
+}
+
+resource "aws_subnet" "Private-subnet" {
+  vpc_id = aws_vpc.vpc-1.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags={
+    Name="Private-Subnet"
+  }
+}
+
+resource "aws_subnet" "Public-subnet" {
+  vpc_id            =       aws_vpc.vpc-1.id
+  cidr_block        =       "10.0.2.0/24"
+  availability_zone =       "us-east-1b"
+  map_public_ip_on_launch = true
+  tags={
+    Name="Public-Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "My-igw" {
+  vpc_id = aws_vpc.vpc-1.id
+  tags = {
+    Name="My-igw"
+  }
+}
+
+resource "aws_route_table" "My-RT" {
+  vpc_id = aws_vpc.vpc-1.id
+  route{
+    gateway_id = aws_internet_gateway.My-igw.id
+    cidr_block = "0.0.0.0/0"
+  }
+  tags={
+    Name="My-RT"
+  }
+}
+
+resource "aws_route_table_association" "RT-Subnet-Association" {
+  subnet_id      = aws_subnet.Public-subnet.id
+  route_table_id = aws_route_table.My-RT.id
+}
