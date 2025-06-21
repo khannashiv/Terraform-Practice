a7fe3a809dcb1471510659e384469d096e380d1a
Shiv <khannashiv94@gmail.com>
2025-06-21 12:17:49 +0530

    test

--- Projects/Project-1/user_data_web_1.sh
+++ Projects/Project-1/user_data_web_1.sh
@@ -19,15 +19,47 @@ REGION=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/la
 HOSTNAME=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/hostname)
 
 # Write HTML
+# Write HTML
 cat <<EOF > /var/www/custom-web/index.html
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Welcome to my Terraform Project!</title>
+  <title>Terraform EC2 Metadata</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f8fb;
+      color: #333;
+      padding: 20px;
+    }
+    h1 {
+      color: #007acc;
+    }
+    h2 {
+      color: #005b99;
+      border-bottom: 2px solid #007acc;
+      padding-bottom: 5px;
+    }
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    li {
+      background-color: #ffffff;
+      margin: 8px 0;
+      padding: 10px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+    }
+    strong {
+      color: #000;
+    }
+  </style>
 </head>
 <body>
-  <h1>Hello from Web Server 1..!!</h1>
-  <p>This server is running Python's built-in HTTP module.</p>
+  <h1>Hello from Web Server 1 🚀</h1>
+  <p>This server is running Python's built-in HTTP module and powered by Terraform.</p>
+
   <h2>Instance Metadata</h2>
   <ul>
     <li><strong>Instance ID:</strong> $INSTANCE_ID</li>
@@ -41,6 +73,7 @@ cat <<EOF > /var/www/custom-web/index.html
 </body>
 </html>
 EOF
+
 
 # Launch simple HTTP server
 nohup python3 -m http.server 80 --directory /var/www/custom-web &
