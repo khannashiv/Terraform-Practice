fc44354103d54d4b68bebe61953d1bb444715e03
Shiv <khannashiv94@gmail.com>
2025-06-21 12:03:13 +0530

    test

--- Projects/Project-1/user_data_web_1.sh
+++ Projects/Project-1/user_data_web_1.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 
-# Update package list and upgrade packages
 apt-get update -y
-apt-get upgrade -y
+apt-get install -y python3 curl jq
 
-# Install Python3 if not already installed
-apt-get install -y python3
-
-# Create a directory to host the website
 mkdir -p /var/www/custom-web
 
-# Create a basic HTML file
+# Request IMDSv2 token
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+# Fetch instance metadata
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+AMI_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/ami-id)
+INSTANCE_TYPE=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-type)
+PRIVATE_IP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
+AVAILABILITY_ZONE=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone)
+REGION=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+HOSTNAME=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/hostname)
+
+# Write HTML
 cat <<EOF > /var/www/custom-web/index.html
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Welcome to my Terraform Project !!</title>
+  <title>Welcome to my Terraform Project!</title>
 </head>
 <body>
-  <h1>Hello from web server 1 ..!!</h1>
-  <p>This is a custom web server using Python's built-in HTTP module.</p>
+  <h1>Hello from Web Server 1..!!</h1>
+  <p>This server is running Python's built-in HTTP module.</p>
+  <h2>Instance Metadata</h2>
+  <ul>
+    <li><strong>Instance ID:</strong> $INSTANCE_ID</li>
+    <li><strong>AMI ID:</strong> $AMI_ID</li>
+    <li><strong>Instance Type:</strong> $INSTANCE_TYPE</li>
+    <li><strong>Private IP:</strong> $PRIVATE_IP</li>
+    <li><strong>Availability Zone:</strong> $AVAILABILITY_ZONE</li>
+    <li><strong>Region:</strong> $REGION</li>
+    <li><strong>Hostname:</strong> $HOSTNAME</li>
+  </ul>
 </body>
 </html>
 EOF
 
-# Start the web server on port 80 using Python (in the background)
+# Launch simple HTTP server
 nohup python3 -m http.server 80 --directory /var/www/custom-web &
-
-
--- Projects/Project-1/user_data_web_2.sh
+++ Projects/Project-1/user_data_web_2.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# Update package list and upgrade packages
 apt-get update -y
-apt-get upgrade -y
+apt-get install -y python3 curl jq
 
-# Install Python3 and curl if not already installed
-apt-get install -y python3 curl
-
-# Create a directory to host the website
 mkdir -p /var/www/custom-web
 
-# Fetch instance metadata
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-AMI_ID=$(curl -s http://169.254.169.254/latest/meta-data/ami-id)
-INSTANCE_TYPE=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
-PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-AVAILABILITY_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/hostname)
-REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
+# Request IMDSv2 token
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
-# Create a dynamic HTML file
+# Fetch instance metadata
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+AMI_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/ami-id)
+INSTANCE_TYPE=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-type)
+PRIVATE_IP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
+AVAILABILITY_ZONE=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone)
+REGION=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+HOSTNAME=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/hostname)
+
+# Write HTML
 cat <<EOF > /var/www/custom-web/index.html
 <!DOCTYPE html>
 <html>
@@ -28,9 +27,8 @@ cat <<EOF > /var/www/custom-web/index.html
 </head>
 <body>
   <h1>Hello from Web Server 2!</h1>
-  <p>This is a custom web server using Python's built-in HTTP module.</p>
-
-  <h2>Instance Metadata:</h2>
+  <p>This server is running Python's built-in HTTP module.</p>
+  <h2>Instance Metadata</h2>
   <ul>
     <li><strong>Instance ID:</strong> $INSTANCE_ID</li>
     <li><strong>AMI ID:</strong> $AMI_ID</li>
@@ -44,8 +42,5 @@ cat <<EOF > /var/www/custom-web/index.html
 </html>
 EOF
 
-# Start the web server on port 80 using Python (in the background)
+# Launch simple HTTP server
 nohup python3 -m http.server 80 --directory /var/www/custom-web &
-
-
-
