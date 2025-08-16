a324af3ee15bf98662810c2f445acac0271e50d1
Shiv <khannashiv94@gmail.com>
2025-08-16 13:35:24 +0530

    test

--- Read-generate-modify-config/output.tf
+++ Read-generate-modify-config/output.tf
@@ -9,8 +9,8 @@ resource "aws_eip" "demo_eip" {
 output "elastic_public_ip" {
 
 # value = aws_eip.demo_eip.public_ip                      # M1 : Presenting output of public IP which is one of the attribute of EIP. 
-value = "https://${aws_eip.demo_eip.public_ip}:8080"    # M2 : Presenting output in the form of URL.
-# value = aws_eip.demo_eip                                  # M3 : To display all the attributes associated with a resource.
+value = "https://${aws_eip.demo_eip.public_ip}:8080"      # M2 : Presenting output in the form of URL.
+# value = aws_eip.demo_eip                                # M3 : To display all the attributes associated with a resource.
 
 }
 
