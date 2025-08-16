a7c4ce63f2fd7d4f9b752622e471e62f7849f486
Shiv <khannashiv94@gmail.com>
2025-08-16 13:34:40 +0530

    test

--- Read-generate-modify-config/attributes.tf
+++ Read-generate-modify-config/attributes.tf
@@ -1,15 +1,15 @@
 # Reading attributes from terrafrom state file & matching those attributes by going to aws UI. Such as:
 # instance_id, public_ip, private_ip, dns, id etc.
 
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
-resource "aws_instance" "web" {
-    ami = "ami-020cba7c55df1f615"
-    instance_type = "t2.micro"
-}
+# resource "aws_instance" "web" {
+#     ami = "ami-020cba7c55df1f615"
+#     instance_type = "t2.micro"
+# }
 
-resource "aws_eip" "static_ip" {
-    domain = "vpc"    
-}
+# resource "aws_eip" "static_ip" {
+#     domain = "vpc"    
+# }
