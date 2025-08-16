684da7519b388e35ccfb62deedd89a74d4d0e80a
Shiv <khannashiv94@gmail.com>
2025-08-16 11:55:21 +0530

    test

---
+++ Read-generate-modify-config/old-vs-new-config/old-config-sg.tf
@@ -0,0 +1,32 @@
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "old"
+}
+
+resource "aws_security_group" "allow_tls_old" {
+  provider = aws.old
+  name        = "allow_tls_old"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls_old"
+  }
+}
