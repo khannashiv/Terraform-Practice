bf2531d845241cef1c12cc14dcddf86dd7b62864
Shiv <khannashiv94@gmail.com>
2025-08-16 12:19:52 +0530

    "Reading attributes from state file"

---
+++ Read-generate-modify-config/attributes.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-020cba7c55df1f615"
+    instance_type = "t2.micro"
+}
+
+resource "aws_eip" "static_ip" {
+    domain = "vpc"    
+}
