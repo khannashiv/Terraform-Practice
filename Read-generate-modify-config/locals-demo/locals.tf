7137b58ac84a34d6cb3dcfca0e9cd931b0392131
Shiv <khannashiv94@gmail.com>
2025-08-19 06:18:37 +0530

    test

---
+++ Read-generate-modify-config/locals-demo/locals.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  my_local_sg = {                               # Any name can be given to locals block ( local values ).
+    name = "firewall-sg"                        # Here in this case name is given as: my_local_sg
+  }
+}
+
+resource "aws_security_group" "sg-1" {
+  name = "app-security-group"
+  tags = local.my_local_sg
+}
+
+resource "aws_security_group" "sg-2" {
+  name = "db-security-group"
+  tags = local.my_local_sg
+}
