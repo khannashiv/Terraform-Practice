846def2aec758512a19f5b549dd01e026af052e0
Shiv <khannashiv94@gmail.com>
2025-08-19 07:43:18 +0530

    test

--- Read-generate-modify-config/data-sources-demo/data-source-1.tf
+++ Read-generate-modify-config/data-sources-demo/data-source-1.tf
@@ -1,8 +1,8 @@
 # Method-1 (Working)
 
 data "local_file" "foo" {
-  filename = "${path.module}/README.md"
-}
+  filename = "${path.module}/README.md"         # ${path.module} : This will redirect to current working directory or current folder.
+}                                               # Rather than defining the absolute path of README.md
 
 output "display_local_file_content" {
   value = data.local_file.foo.content
