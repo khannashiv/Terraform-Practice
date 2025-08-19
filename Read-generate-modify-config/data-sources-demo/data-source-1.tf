a5b51bc1c840c4f1f3f4c3eb32c2791488fa86c4
Shiv <khannashiv94@gmail.com>
2025-08-19 07:24:48 +0530

    test

--- Read-generate-modify-config/data-sources-demo/data-source-1.tf
+++ Read-generate-modify-config/data-sources-demo/data-source-1.tf
@@ -1,11 +1,11 @@
-# Method-1
+# Method-1 (Working)
 
 data "local_file" "foo" {
   filename = "${path.module}/README.md"
 }
 
-# Method-2
+# Method-2 (Working)
 
-data "local_file" "my_file" {
-  filename = "./README.md"
-}
+# data "local_file" "my_file" {
+#   filename = "./README.md"
+# }
