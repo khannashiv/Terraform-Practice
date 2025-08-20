550930a0680fbe6bf41416e65f940999febebb26
Shiv <khannashiv94@gmail.com>
2025-08-20 07:42:19 +0530

    test

--- Read-generate-modify-config/data-sources-demo/data-source-4.tf
+++ Read-generate-modify-config/data-sources-demo/data-source-4.tf
@@ -21,7 +21,7 @@ data "aws_ami" "example" {
   owners = ["amazon"]
   filter {
     name   = "name"
-    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-lunar-23.04-amd64-minimal-*"]
+    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-lunar-23.04-amd64-minimal-*"] # To pull latest ami / image
   }
 }
 
