79de7589e2f149d059589e71b46306f929425e8c
Shiv <khannashiv94@gmail.com>
2025-08-21 09:27:25 +0530

    test

--- Read-generate-modify-config/spalat-demo/spalat.tf
+++ Read-generate-modify-config/spalat-demo/spalat.tf
@@ -1,6 +1,11 @@
 # Refrence Docs: 
     # https://developer.hashicorp.com/terraform/language/expressions/splat
     # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_user
+    # https://developer.hashicorp.com/terraform/language/values/outputs
+    # https://developer.hashicorp.com/terraform/cli/commands/output
+    # Some commands used are:
+      # terraform output user_arns
+      # terraform output "user_names"
 
 provider "aws" {
   region = "us-east-1"
@@ -11,8 +16,12 @@ resource "aws_iam_user" "lb" {
   count = 3
 }
 
-output "arns" {
+output "user_arns" {
 #  value = aws_iam_user.lb[0].arn   # Will display arn for user at 0th index
 #  value = aws_iam_user.lb[1].arn   # Will display arn for user at 1st index
   value = aws_iam_user.lb[*].arn    # [*] -- > This is spalat function which will display arn of all the users created using terraform apply.
+}
+
+output "user_names" {
+  value = aws_iam_user.lb[*].name
 }
