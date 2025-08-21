8f9af145d729842c35d38bf9bc656ee1fa8e2849
Shiv <khannashiv94@gmail.com>
2025-08-21 09:12:16 +0530

    test

--- Read-generate-modify-config/saving-plan.demo/main.tf
+++ Read-generate-modify-config/saving-plan.demo/main.tf
@@ -1,9 +1,20 @@
 # Demo: Saving terraform plan to new file.
+
+    # Create a infra first using terraform.
+    # for example: write main.tf >> terraform plan >> terraform apply >> After this create infra.plan
+    # Make changes to main.tf >> do terraform apply "infra.plan" >> New chnages done under main.tf will
+    #  not take effect
+
     # Commands used are:
         # terraform plan -out plan.infra
         # terraform apply infra.plan or terraform apply "infra.plan"
             # This means whenever we are going to replace old file with new file, new file along with 
             # it's content will not be (created / updated) since we are applying "apply" operation on saved plan
+        # terraform show "infra.plan"       ---> To view the content of infra.plan file since this is a binary file, we won't able to see content of this file on VS code.
+        # terraform show -json infra.plan   ---> To see ouput in json format
+        # https://jsonformatter.org/        ---> Online json formatter
+        # As part of best practices, prefer to install jq natively on your system
+        # terraform show -json "infra.plan" | jq
 
 resource "local_file" "demo" {
     # filename = "sample.txt"                                                                 # Initial file
