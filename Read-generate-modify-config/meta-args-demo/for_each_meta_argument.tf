c42a589772d65982af0de612a1e16843373abd40
Shiv <khannashiv94@gmail.com>
2025-08-23 13:55:58 +0530

    test

--- Read-generate-modify-config/meta-args-demo/for_each_meta_argument.tf
+++ Read-generate-modify-config/meta-args-demo/for_each_meta_argument.tf
@@ -1,20 +1,20 @@
 # Example 1 : Implamenting for_each on set data type.
 # Refrence Docs : https://developer.hashicorp.com/terraform/language/meta-arguments/for_each
 
-# provider "aws" {
-#   region = "us-east-1"
-# }
+provider "aws" {
+  region = "us-east-1"
+}
 
-# variable "demo-set" {
-#   type = set(string)
-#   default = ["User-1", "User-2", "User-3", "User-4", "User-5"]
-# }
+variable "demo-set" {
+  type = set(string)
+  default = ["User-1", "User-2", "User-3", "User-4", "User-5"]
+}
 
-# resource "aws_iam_user" "IAM_Users" {
-#   for_each = var.demo-set
-#   name = each.key
-#   path = "/"
-# }
+resource "aws_iam_user" "IAM_Users" {
+  for_each = var.demo-set
+  name = each.key # Here one can also specify each.value but overall output will remain same.
+  path = "/"
+}
 
 # Example 2 : Implamenting for_each on map data type.
 
