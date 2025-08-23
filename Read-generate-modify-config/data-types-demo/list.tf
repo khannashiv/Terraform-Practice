5fde2aed5b8c6e853e266385856d19a08150d14b
Shiv <khannashiv94@gmail.com>
2025-08-23 12:59:27 +0530

    test

--- Read-generate-modify-config/data-types-demo/list.tf
+++ Read-generate-modify-config/data-types-demo/list.tf
@@ -1,13 +1,14 @@
 # List : is ordered collection of items. 
+
 # Test this behavior by changing order / index value of elements say: placing last element
 # in the start of list and vice-versa. Then go with terraform plan >> followed by terraform
 # apply.
 
-variable "list-1" {
-  type = list(string)
-  default = ["Raj", "Alice", "John", "Bob"]
-}
+# variable "list-1" {
+#   type = list(string)
+#   default = ["Raj", "Alice", "John", "Bob"]
+# }
 
-output "list-1" {
-  value = var.list-1
-}
+# output "list-1" {
+#   value = var.list-1
+# }
--- Read-generate-modify-config/data-types-demo/set.tf
+++ Read-generate-modify-config/data-types-demo/set.tf
@@ -1,14 +1,14 @@
-# Set : Un-ordered collection of items. Further set data type will remove duplicates as well.
+# Set : Un-ordered collection of items.Further set data type will remove duplicates as well.
 
-# Test un-ordered collection behavior by changing order / index value of elements say: placing last element
-# in the start of list and vice-versa. Then go with terraform plan >> followed by terraform
-# apply.
+# Test un-ordered collection behavior of set data-type by changing order / index value of elements say:
+# placing last element in the start of list and vice-versa. Then go with terraform plan >> followed by 
+# terraform apply. We will see order does not matter in this case.
 
-# variable "set-1" {
-#   type = set(string)
-#   default = ["Alice", "John", "Bob", "Alice"]
-# }
+variable "set-1" {
+  type = set(string)
+  default = ["Alice", "Bob", "John", "Alice"]
+}
 
-# output "set-1" {
-#   value = var.set-1
-# }
+output "set-1" {
+  value = var.set-1
+}
