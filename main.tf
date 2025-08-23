8bf376feec6b1c62fdd78e59a688bdd26f091f97
Shiv <khannashiv94@gmail.com>
2025-08-23 18:59:29 +0530

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
-provider "aws" {
-  region = var.region
+# Change 2: Instead of hardcoding provider block we are going to use required provider block so that we can add
+# additional constraints w.r.t. provider block such that we can add or set version to the given provider (Say
+# the code, I have written works pretty well with specific version of provider plugin say v6.6 now whenever some-one
+# else would like to use my code only that specific verison of terraform provider plugin should be installed during 
+# terraform init to avoid any failure like this we are fixing the constraints) & avoiding hardcoding of region 
+# as parameter as well.
+
+# provider "aws" {
+#   region = var.region
+# }
+
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">=6.0.0"
+    }
+  }
 }
 
 resource "aws_instance" "web" {
@@ -7,6 +23,8 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
 }
 
-variable "region" {}
+# Change 1 : Using variables instead of static values for region, ami, instance_type.
+
+# variable "region" {}
 variable "ami" {}
 variable "instance_type" {}
