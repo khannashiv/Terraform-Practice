7da1b423b8a56e40e4576c5709f980dabcab83d6
Shiv <khannashiv94@gmail.com>
2025-08-23 11:37:36 +0530

    test

--- Read-generate-modify-config/meta-args-demo/depends_on_meta_argument.tf
+++ Read-generate-modify-config/meta-args-demo/depends_on_meta_argument.tf
@@ -9,5 +9,8 @@ resource "aws_s3_bucket" "demo-bucket" {
 resource "aws_instance" "demo-ec2" {
   instance_type = "t1.micro"
   ami = "ami-0360c520857e3138f"
-  depends_on = [ aws_s3_bucket.demo-bucket ] # This means
+  depends_on = [ aws_s3_bucket.demo-bucket ] # This means first s3 bucket is created & 
+  # later ec2 instance is created due to depends_on flag.
+  # NOTE: Destruction of resources will be done in reverse way where ec2 will be destroyed
+  # first & later s3 bucket. 
 }
