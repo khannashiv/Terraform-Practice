f8da7451bd4cd2ecedcccc3dc49536847f74ea7e
Shiv <khannashiv94@gmail.com>
2025-08-23 10:47:39 +0530

    test

--- Read-generate-modify-config/meta-args-demo/prevent_destroy_meta_argument.tf
+++ Read-generate-modify-config/meta-args-demo/prevent_destroy_meta_argument.tf
@@ -8,13 +8,13 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   ami = "ami-0fd3ac4abb734302a"     # Redhat AMI : New
   tags = {
-    Name = "Web_Instance"
+    Name = "Web_Instance_RedHat"
   }
 
-#   lifecycle {
-#     prevent_destroy = true # Here as per the default behaviour whenever we go ahead & use 
-#     # terraform destroy it is going to destroy infrastructure but as aoon as we go head & use
-#     # prevent destroy meta-arg it will prevent destruction operation.
-#   }
+  lifecycle {
+    prevent_destroy = true # Here as per the default behaviour whenever we go ahead & use 
+    # terraform destroy it is going to destroy infrastructure but as aoon as we go head & use
+    # prevent destroy meta-arg it will prevent destruction operation.
+  }
 
 }
