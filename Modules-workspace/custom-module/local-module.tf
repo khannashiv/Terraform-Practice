bad976c3c414aba65e6d175e51c2a15561583436
Shiv <khannashiv94@gmail.com>
2025-08-23 18:02:42 +0530

    test

---
+++ Modules-workspace/custom-module/local-module.tf
@@ -0,0 +1,10 @@
+module "custom_ec2_local" {
+
+  # Relative-Path working.
+  source = "../../Read-generate-modify-config/graph-demo"
+
+  # Absolute Path: D:\Git-repository\Terraform-Practice\Read-generate-modify-config\graph-demo
+
+  # Absolute-Path working.  
+  # source = "D:/Git-repository/Terraform-Practice/Read-generate-modify-config/graph-demo"
+}
