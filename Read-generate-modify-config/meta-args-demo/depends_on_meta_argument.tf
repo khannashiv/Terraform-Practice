166f68655f1d565f0d888496c0c992930b51943f
Shiv <khannashiv94@gmail.com>
2025-08-23 11:35:00 +0530

    test

--- Read-generate-modify-config/meta-args-demo/depends_on_meta_argument.tf
+++ Read-generate-modify-config/meta-args-demo/depends_on_meta_argument.tf
@@ -5,3 +5,9 @@ provider "aws" {
 resource "aws_s3_bucket" "demo-bucket" {
   bucket = "my-terraform-learning-bucket"
 }
+
+resource "aws_instance" "demo-ec2" {
+  instance_type = "t1.micro"
+  ami = "ami-0360c520857e3138f"
+  depends_on = [ aws_s3_bucket.demo-bucket ] # This means
+}
