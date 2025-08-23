7c4542683a197aac5b8a4510b7eee4c1f63be2b8
Shiv <khannashiv94@gmail.com>
2025-08-23 11:46:02 +0530

    test

--- Read-generate-modify-config/meta-args-demo/depends_on_meta_argument.tf
+++ Read-generate-modify-config/meta-args-demo/depends_on_meta_argument.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "demo-bucket" {
-  bucket = "my-terraform-learning-bucket"
+  bucket = "my-terraform-learning-bucket-001"
 }
 
 resource "aws_instance" "demo-ec2" {
@@ -14,3 +14,32 @@ resource "aws_instance" "demo-ec2" {
   # NOTE: Destruction of resources will be done in reverse way where ec2 will be destroyed
   # first & later s3 bucket. 
 }
+
+
+
+# Output looks something like shown below.
+
+    # Case 1 : Creating resources.
+
+            # On running terraform apply -auto-approve, this will show output something like shown below.
+
+                    # # Plan: 2 to add, 0 to change, 0 to destroy.
+                    # aws_s3_bucket.demo-bucket: Creating...
+                    # aws_s3_bucket.demo-bucket: Creation complete after 5s [id=my-terraform-learning-bucket-001]
+                    # aws_instance.demo-ec2: Creating...
+                    # aws_instance.demo-ec2: Still creating... [00m10s elapsed]
+                    # aws_instance.demo-ec2: Creation complete after 16s [id=i-06eda4297dc8cde86]
+
+    # Case 2 : Destroying resourcse.
+
+            # # On running terraform destroy -auto-approve, this will show output something like shown below.
+
+                # Plan: 0 to add, 0 to change, 2 to destroy.
+                # aws_instance.demo-ec2: Destroying... [id=i-06eda4297dc8cde86]
+                # aws_instance.demo-ec2: Still destroying... [id=i-06eda4297dc8cde86, 00m10s elapsed]
+                # aws_instance.demo-ec2: Still destroying... [id=i-06eda4297dc8cde86, 00m20s elapsed]
+                # aws_instance.demo-ec2: Still destroying... [id=i-06eda4297dc8cde86, 00m30s elapsed]
+                # aws_instance.demo-ec2: Destruction complete after 32s
+                # aws_s3_bucket.demo-bucket: Destroying... [id=my-terraform-learning-bucket-001]
+                # aws_s3_bucket.demo-bucket: Destruction complete after 1s
+                # Destroy complete! Resources: 2 destroyed.
