df82488c02322ab591d06a53a62722997ab7fedf
Shiv <khannashiv94@gmail.com>
2025-08-23 12:53:14 +0530

    test

--- Read-generate-modify-config/data-types-demo/set.tf
+++ Read-generate-modify-config/data-types-demo/set.tf
@@ -1,4 +1,8 @@
-# Set : Un-ordered collection of items.
+# Set : Un-ordered collection of items. Further set data type will remove duplicates as well.
+
+# Test un-ordered collection behavior by changing order / index value of elements say: placing last element
+# in the start of list and vice-versa. Then go with terraform plan >> followed by terraform
+# apply.
 
 # variable "set-1" {
 #   type = set(string)
