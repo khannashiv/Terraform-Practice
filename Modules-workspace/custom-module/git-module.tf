b5b046bfefe6dee6ab622355920236edfeed746b
Shiv <khannashiv94@gmail.com>
2025-08-23 18:19:52 +0530

    test

--- Modules-workspace/custom-module/git-module.tf
+++ Modules-workspace/custom-module/git-module.tf
@@ -1,5 +1,6 @@
 module "git-module" {
   source = "github.com/khannashiv/Terraform-Practice/" # This will look for terraform files (i.e. .tf files) 
   # which are present at the root of the repository.Hence we have created sample main.tf at the root of
-  # the repository.   
+  # the repository.
+  # Initially we have tested by passing static values for region, instance_type & ami
 }
