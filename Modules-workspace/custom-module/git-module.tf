9cdf7c7435c78fc9317ef68daab7d5ad25202759
Shiv <khannashiv94@gmail.com>
2025-08-23 18:24:43 +0530

    test

--- Modules-workspace/custom-module/git-module.tf
+++ Modules-workspace/custom-module/git-module.tf
@@ -2,5 +2,12 @@ module "git-module" {
   source = "github.com/khannashiv/Terraform-Practice/" # This will look for terraform files (i.e. .tf files) 
   # which are present at the root of the repository.Hence we have created sample main.tf at the root of
   # the repository.
-  # Initially we have tested by passing static values for region, instance_type & ami
+
+  # Initially we have tested by passing static values for region, instance_type & ami under main.tf which is
+  # sitting at the root of the repository.
+
+  # Now we are making use of variables such that we are replacing static values under main.tf with variables.
+  ami = "ami-0fd3ac4abb734302a"
+  region = "us-east-1"
+  instance_type="t2.micro"
 }
