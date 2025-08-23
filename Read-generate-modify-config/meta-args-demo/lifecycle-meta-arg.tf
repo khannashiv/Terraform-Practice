6f89275a273bc525a2f1a7b3c49bbefc0a9c59f0
Shiv <khannashiv94@gmail.com>
2025-08-23 10:12:28 +0530

    test

--- Read-generate-modify-config/meta-args-demo/lifecycle-meta-arg.tf
+++ Read-generate-modify-config/meta-args-demo/lifecycle-meta-arg.tf
@@ -1,3 +1,5 @@
+# Meta-argument-1 : Ignore_Changes
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -10,7 +12,9 @@ resource "aws_instance" "web" {
   }
 
   lifecycle {
-    ignore_changes = [ tags ]
+    ignore_changes = [ tags ] # Here default behavior of the terraform has been customnized such that 
+    # now anyone who is trying to add tags to this insatnce from UI will able to add tags whereas default
+    # behaviour of terraform would have deleted the tags i.e. if any modifications made to aws resource from aws UI.
   }
 
 }
