2d741798b1622d5369afff85d261dcf89f11f7d3
Shiv <khannashiv94@gmail.com>
2025-08-23 17:42:24 +0530

    test

--- Modules-workspace/custom-module/git-module.tf
+++ Modules-workspace/custom-module/git-module.tf
@@ -1,3 +1,5 @@
 module "git-module" {
-  source = "github.com/khannashiv/Terraform-Practice/"
+  source = "github.com/khannashiv/Terraform-Practice/" # This will look for terraform files (i.e. .tf files) 
+  # which are present at the root of the repository.  Hence we have created sample main.tf at the root of
+  # the repository.   
 }
