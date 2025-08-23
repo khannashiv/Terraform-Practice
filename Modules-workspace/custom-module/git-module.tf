912e3d556509e528a9e7f8baa9f098690d00f5cd
Shiv <khannashiv94@gmail.com>
2025-08-23 18:32:29 +0530

    test

--- Modules-workspace/custom-module/git-module.tf
+++ Modules-workspace/custom-module/git-module.tf
@@ -7,7 +7,7 @@ module "git-module" {
   # sitting at the root of the repository.
 
   # Now we are making use of variables such that we are replacing static values under main.tf with variables.
-  ami           = "ami-0fd3ac4abb734302a"
-  region        = "us-east-1"
+  ami           = "ami-064fad1cd2e859c98"
+  region        = "ap-south-1"
   instance_type = "t2.micro"
 }
