3334df72672b40b9f15a77d4761abe89145f16a7
Shiv <khannashiv94@gmail.com>
2025-08-23 10:36:18 +0530

    test

---
+++ Read-generate-modify-config/meta-args-demo/prevent_destroy_meta_argument.tf
@@ -0,0 +1,22 @@
+# Meta-argument-3 : prevent-destroy
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  instance_type = "t2.micro"
+# ami = "ami-0360c520857e3138f"     # Ubuntu AMI : Initial
+  ami = "ami-0fd3ac4abb734302a"     # Redhat AMI : New // Final
+  tags = {
+    Name = "Web_Instance"
+  }
+
+  lifecycle {
+    create_before_destroy = true # Here as per the default behaviour of terraform it is going to
+    # delete resource first & later it will add new resource if we update ami-id of the instance
+    # But since we are using create_before_destroy meta-argument in that case instance will be
+    # first and late rit will go ahead & destroy the resource.
+  }
+
+}
