1d70b00bddee26d231bf0d3a65f05d284e2163e9
Shiv <khannashiv94@gmail.com>
2025-08-23 13:19:26 +0530

    test

--- Read-generate-modify-config/meta-args-demo/for_each_meta_argument.tf
+++ Read-generate-modify-config/meta-args-demo/for_each_meta_argument.tf
@@ -6,3 +6,9 @@ variable "demo-set" {
   type = set(string)
   default = ["User-1", "User-2", "User-3", "User-4", "User-5"]
 }
+
+resource "aws_iam_user" "IAM_Users" {
+  for_each = var.demo-set
+  name = each.key
+  path = "/"
+}
