721e63415a6c174f8e29e68cbfe6af896a1e9188
Shiv <khannashiv94@gmail.com>
2025-08-25 09:13:09 +0530

    test

--- Modules-workspace/workspace/workspace.tf
+++ Modules-workspace/workspace/workspace.tf
@@ -4,12 +4,14 @@ provider "aws" {
 
 locals {
   workspace_instance_type = { # workspace_instance_type --> This name can be anything.
-     dev = "t2.micro"
-     prod = "t3.micro"
+    dev  = "t2.micro"
+    prod = "t3.micro"
   }
 }
+
 resource "aws_instance" "web" {
-instance_type = local.workspace_instance_type[terraform.workspace]
-ami = "ami-0360c520857e3138f"
-#   instance_type = [terraform.workspace]
+  instance_type = local.workspace_instance_type[terraform.workspace]
+  ami           = "ami-0360c520857e3138f"
+  # instance_type = "t2.micro" # Tested by hardcoding the value for instance type & confirmed for each 
+  # workspace we are getting diffrent state files.
 }
