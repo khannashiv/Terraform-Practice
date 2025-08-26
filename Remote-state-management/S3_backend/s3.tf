d11bce9995495398f3fd95fb8d5d28240a14b44a
Shiv <khannashiv94@gmail.com>
2025-08-26 07:53:04 +0530

    test

--- Remote-state-management/S3_backend/s3.tf
+++ Remote-state-management/S3_backend/s3.tf
@@ -3,6 +3,6 @@ terraform {
     bucket       = "backend-s3-demo-bucket-01" # NOTE : Bucket should exists on AWS with unique name.
     key          = "Pre-prod.tfstate"          # This file will be created inside S3 (at the root of S3) bucket not locally.
     region       = "us-east-1"
-    use_lockfile = true
+    use_lockfile = true                        # For managing locking mechanism.
   }
 }
