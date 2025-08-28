b5c1d77fe709b8273111baa4b655c5e4a2861550
Shiv <khannashiv94@gmail.com>
2025-08-28 08:11:34 +0530

    test

--- Security/hcp-cli-driven-wf-demo/cli-driven-wf.tf
+++ Security/hcp-cli-driven-wf-demo/cli-driven-wf.tf
@@ -9,14 +9,14 @@ terraform {
   } 
 }
 
-resource "time_sleep" "sleep_func" {
-  create_duration = "30s"
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-0360c520857e3138f"
   instance_type = "t2.micro"
-} 
+}
+
+resource "time_sleep" "sleep_func" {
+  create_duration = "30s"
+}
 
 output "ec2_public_IP" {
   value = aws_instance.web.public_ip
